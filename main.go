9b1de36ea7c68d3e4428bdb97e175a8ab4581229
Zehua Lu <analysek4@gmail.com>
2018-08-20 16:57:26 +0800

    把config相关抽成单独一个包

--- main.go
+++ main.go
@@ -2,35 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
 	"net/http"
-	"strconv"
+	"reverse_proxy_demo/config"
 )
 
-func getEnv(key, fallback string) string {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	viper.SetConfigType("yaml")
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("fatal error config %s \n", err))
-	}
-	tmpport := viper.Get("PORT").(int)
-	port := strconv.Itoa(tmpport)
-	if port == "" {
-		return fallback
-	}
-	return port
-}
 
-func getListenAddress() string {
-	port := getEnv("PORT", "9043")
-	return ":" + port
-}
-
-func LogSetup() {
-
-}
 
 func requestAndRedirect(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("In func requestAndRedirect")
@@ -38,12 +14,9 @@ func requestAndRedirect(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	//log setup values
-	LogSetup()
+	config.LogSetup()
 
 	//start server
 	http.HandleFunc("/", requestAndRedirect)
-	http.ListenAndServe(getListenAddress(), nil)
-
-	//test
-	//fmt.Println(getListenAddress())
+	http.ListenAndServe(config.GetListenAddress(), nil)
 }
